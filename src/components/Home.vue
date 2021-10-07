58386429d80eec7da490a2fa47f7f9a15e0277f1
Chris Brown <chris@sageworksstudio.com>
2021-10-07 14:28:57 -0700

    added grid and columns

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,42 @@
 <template>
 
-  <ext-toolbar
-    items="[
-      {
-        xtype='button',
-        text='Dashboard',
-      }
-    ]" />
+  <ext-toolbar />
 
-  <ext-grid
-    title="The Grid Title">
-  </ext-grid>
+
+
+  <ext-container
+    padding="10"
+    layout="fit"
+    fitToParent="true"
+    height="100%"
+    width="100%">
+
+    <ext-grid
+      title="The Grid Title"
+      shadow="true"
+      :store="orderstore">
+
+      <ext-column
+        text="ID"
+        flex="1"
+        dataIndex="id"></ext-column>
+      <ext-column
+        text="Full Name"
+        flex="1"
+        dataIndex="full_name"></ext-column>
+      <ext-column
+        text="Order #"
+        flex="1"
+        dataIndex="order_number"></ext-column>
+      <ext-column
+        text="Status"
+        flex="1"
+        dataIndex="status"></ext-column>
+
+    </ext-grid>
+
+  </ext-container>
+  
 
   <ext-button
     text="EWC Button"
@@ -19,6 +45,32 @@
 </template>
 
 <script>
+const orderstore = [
+{
+"id": 1,
+"full_name": "Glen Danzig",
+"order_number": "674K9P",
+"status": "Shipped"
+},
+{
+"id": 2,
+"full_name": "Ru Paul",
+"order_number": "JD782E",
+"status": "Pending"
+},
+{
+"id": 3,
+"full_name": "Buckethead",
+"order_number": "F93DV8",
+"status": "Shipped"
+},
+{
+"id": 4,
+"full_name": "SpongeBob SquarePants",
+"order_number": "IE682X",
+"status": "On Hold"
+}
+]
 export default {
   name: 'Home',
   setup() {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@sencha/ext-web-components-modern/dist/ext-button.component'
-import '@sencha/ext-web-components-modern/dist/ext-grid.component'
 import '@sencha/ext-web-components-modern/dist/ext-toolbar.component'
+import '@sencha/ext-web-components-modern/dist/ext-container.component'
+import '@sencha/ext-web-components-modern/dist/ext-grid.component'
+import '@sencha/ext-web-components-modern/dist/ext-column.component'
 
 Ext.onReady(function() {
   createApp(App).mount('#app')
